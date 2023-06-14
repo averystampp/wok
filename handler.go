7f0bc8e9ff57a2be6d02d00964b7e11801378c52
averystampp <amstampp18@gmail.com>
2023-06-14 12:42:16 -0400

    added redirect to login handler

--- handler.go
+++ handler.go
@@ -53,6 +53,7 @@ func LoginHandle(ctx Context) {
 	cookie.HttpOnly = true
 	cookie.Expires = time.Now().Add(30 * time.Minute).Local()
 	http.SetCookie(ctx.Resp, cookie)
+	http.Redirect(ctx.Resp, ctx.Req, "/home", 200)
 
 }
 
