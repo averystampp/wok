62b090e3707336e4db6d6f121e522dbda9eba9f7
averystampp <amstampp18@gmail.com>
2023-06-26 10:31:37 -0400

    use only one wok.StartWok method, for both TLS and unsecure

--- server.go
+++ server.go
@@ -54,6 +54,7 @@ func (w *Wok) StartWok(db DbConfig) {
 	if err := connectToDB(&db); err != nil {
 		panic(err)
 	}
+	defer Database.Close()
 
 	fmt.Println(WOK_VERSION)
 	fmt.Printf("---------------------------------\n| Server starting on port %s |\n---------------------------------\n", w.address)
@@ -61,38 +62,10 @@ func (w *Wok) StartWok(db DbConfig) {
 	// call the default router
 	DefaultRouter(w)
 
-	http.ListenAndServe(w.address, w.mux)
-	defer Database.Close()
-}
-
-func (w *Wok) StartWokTLS(db DbConfig) {
-	if err := validatedbconfig(db); err != nil {
-		panic(err)
+	if w.certFile != "" && w.keyFile != "" {
+		http.ListenAndServeTLS(w.address, w.certFile, w.keyFile, w.mux)
+	} else {
+		http.ListenAndServe(w.address, w.mux)
 	}
 
-	for _, arg := range os.Args {
-		if arg == "createuser" {
-			newAdmin(&db)
-			os.Exit(0)
-		}
-
-		if arg == "droptable" {
-			dropTable(&db)
-			os.Exit(0)
-		}
-	}
-
-	if err := connectToDB(&db); err != nil {
-		panic(err)
-	}
-	fmt.Println(WOK_VERSION)
-	fmt.Println("-------------------------------------")
-	fmt.Printf("| Server starting w/TLS on port %s |\n", w.address)
-	fmt.Println("-------------------------------------")
-
-	// call the default router
-	DefaultRouter(w)
-
-	http.ListenAndServeTLS(w.address, w.certFile, w.keyFile, w.mux)
-	defer Database.Close()
 }
